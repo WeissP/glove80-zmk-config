0ea1e059de545a8ff96f5d06a5543af4297ff797
weiss <kingbaiing@163.com>
2023-11-10 21:16:53 +0100

    add zmk and locale header

--- config/default.nix
+++ config/default.nix
@@ -1,10 +1,17 @@
-{ pkgs ?  import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 let
-  firmware = import ../src {};
+  firmware = import ../zmk { };
   config = ./.;
 
-  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
-  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
+  withBoard = board:
+    firmware.zmk.override {
+      inherit board;
+      keymap = "${config}/glove80.c";
+      kconfig = "${config}/glove80.conf";
+    };
+
+  glove80_left = withBoard "glove80_lh";
+  glove80_right = withBoard "glove80_rh";
 
 in firmware.combine_uf2 glove80_left glove80_right
